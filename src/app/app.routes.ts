632cd245006d421bc891206161640aadb7e43a75
abra25 <solidy789gmail.com>
2026-06-16 04:38:09 -0700

    create home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Home } from './public/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component:Home}
+];
