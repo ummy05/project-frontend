94720d22e9fa5c80c777469cbd4af5d34288ad20
abra25 <solidy789gmail.com>
2026-06-28 15:03:10 -0700

    frontend##97%tmp

--- src/app/auth/forgot-pssword/forgot-pssword.ts
+++ src/app/auth/forgot-pssword/forgot-pssword.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-pssword',
-  imports: [],
+  imports: [CommonModule,FormsModule,RouterLink],
   templateUrl: './forgot-pssword.html',
   styleUrl: './forgot-pssword.css',
 })
 export class ForgotPssword {
+
+  email = '';
+  emailSent = false;
+
+  sendResetLink() {
+    this.emailSent = true;
+  }
 
 }
--- src/app/public/register/register.ts
+++ src/app/public/register/register.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule,CommonModule,RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
